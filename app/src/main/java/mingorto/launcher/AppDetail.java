3b6100a373e2497877752c8f63ead493343bae16
Hitrene <kirill.matrosov979@gmail.com>
2017-07-09 22:12:42 +0300

    Version 0.0.0

---
+++ app/src/main/java/mingorto/launcher/AppDetail.java
@@ -0,0 +1,9 @@
+package mingorto.launcher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/mingorto/launcher/Settings.java
@@ -0,0 +1,7 @@
+package mingorto.launcher;
+
+import android.app.Activity;
+
+public class Settings extends Activity {
+
+}
