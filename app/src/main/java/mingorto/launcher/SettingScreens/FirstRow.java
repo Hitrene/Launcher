6614ee5c089bd4fe9b6450a4ae5b141cdaba0959
Hitrene <kirill.matrosov979@gmail.com>
2017-08-04 00:36:28 +0300

    Version 0.0.51 Добавлен выбор типа меню приложений

--- app/src/main/java/mingorto/launcher/SettingScreens/FirstRow.java
+++ app/src/main/java/mingorto/launcher/SettingScreens/FirstRow.java
@@ -1,27 +1,55 @@
 package mingorto.launcher.SettingScreens;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 
 import mingorto.launcher.R;
 
 public class FirstRow extends Activity {
+    public static final String USER_SETTINGS = "user_settings";
+    public static final String USER_SETTINGS_LAUNCHER_TYPE = "launcher_type";
+    private final String[] launcherType = {"Классический", "Один экран"};
+    private SharedPreferences settings;
+    private SharedPreferences.Editor editor;
+    private AlertDialog.Builder alertDialog;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_row);
 
+        context = FirstRow.this;
+        settings = getSharedPreferences(USER_SETTINGS, Context.MODE_PRIVATE);
+
         Button button = (Button) findViewById(R.id.defLauncher);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() { //Настройка диалогового окна для выбора типа отображения рабочего стола
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_MAIN);
-                i.addCategory(Intent.CATEGORY_LAUNCHER);
-                startActivity(i);
+                alertDialog = new AlertDialog.Builder(context);
+                alertDialog.setTitle("Тип меню приложений").setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }).setSingleChoiceItems(launcherType, settings.getInt(USER_SETTINGS_LAUNCHER_TYPE, 0), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        editor = settings.edit();
+                        editor.putInt(USER_SETTINGS_LAUNCHER_TYPE, which);
+                        editor.apply();
+                        Log.v("New launcher type : ", String.valueOf(settings.getInt(USER_SETTINGS_LAUNCHER_TYPE, 0)));
+                    }
+                });
+                alertDialog.create();
+                alertDialog.show();
             }
         });
     }
--- app/src/main/java/mingorto/launcher/SettingScreens/SecondRow.java
+++ app/src/main/java/mingorto/launcher/SettingScreens/SecondRow.java
@@ -1,7 +1,15 @@
 package mingorto.launcher.SettingScreens;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import mingorto.launcher.R;
 
 public class SecondRow extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_row);
+    }
 
 }
